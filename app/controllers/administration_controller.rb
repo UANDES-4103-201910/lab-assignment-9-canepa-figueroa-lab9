bc6984f4cf90f74b05fefd7a6948af4650f3e4e8
Matias Canepa <macanepa@miuandes.cl>
2019-05-23 20:26:40 -0400

    Added Guest User

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,4 +1,7 @@
 class AdministrationController < ApplicationController
   def index
+    if( not can? :Manage, Event)
+      redirect_to root_path, notice: "User doesn't have the required privilages"
+    end
   end
 end
